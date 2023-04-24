8380a818cd821451153abf7450a349832e0b7c62
pythagoras58 <solomondanso58@gmail.com>
2023-04-24 22:51:28 +0000

    Initial commit

---
+++ src/main/java/com/pythagorasweb/springwithtest/SpringWithTestApplication.java
@@ -0,0 +1,13 @@
+package com.pythagorasweb.springwithtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWithTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pythagorasweb/springwithtest/SpringWithTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pythagorasweb.springwithtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

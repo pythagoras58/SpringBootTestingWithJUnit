0778b4138785d012745f9d0179015ed6769bcbbe
pythagoras58 <solomondanso58@gmail.com>
2023-04-25 21:34:39 +0000

    Utility class - test

--- src/main/java/com/pythagorasweb/springwithtest/Utilities/Calculators.java
+++ src/main/java/com/pythagorasweb/springwithtest/Utilities/Calculators.java
@@ -1,4 +1,8 @@
 package com.pythagorasweb.springwithtest.Utilities;
 
 public class Calculators {
+
+    public int getProducts(int a, int b){
+        return a*b;
+    }
 }
---
+++ src/test/java/com/pythagorasweb/springwithtest/Utilities/CalculatorsTest.java
@@ -0,0 +1,22 @@
+package com.pythagorasweb.springwithtest.Utilities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorsTest {
+
+    // instantiate the calculator class
+    Calculators calculators;
+
+    @BeforeEach
+    void setUp(){
+        calculators = new Calculators();
+    }
+
+    @Test
+    void getProducts() {
+
+    }
+}

d8dd5befcd26450059b7df8a3e8b6f03ea74a7de
pythagoras58 <solomondanso58@gmail.com>
2023-04-25 21:29:51 +0000

    Utility class

---
+++ src/main/java/com/pythagorasweb/springwithtest/Utilities/Calculators.java
@@ -0,0 +1,4 @@
+package com.pythagorasweb.springwithtest.Utilities;
+
+public class Calculators {
+}
--- src/test/java/com/pythagorasweb/springwithtest/SpringWithTestApplicationTests.java
+++ src/test/java/com/pythagorasweb/springwithtest/SpringWithTestApplicationTests.java
@@ -3,11 +3,20 @@ package com.pythagorasweb.springwithtest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class SpringWithTestApplicationTests {
 
     @Test
     void contextLoads() {
+
+    }
+
+
+    @Test
+    public void demoTestMethod(){
+        assertTrue(true);
     }
 
 }
